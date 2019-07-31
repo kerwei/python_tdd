002fc9aa51bcc1a2aadaa88f9420820a0628e614
kerwei <kerwei84@gmail.com>
2019-07-31 16:38:55 +0800

    Apply Bootstrap to layout

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,16 +1,16 @@
-from django.test import LiveServerTestCase
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.common.exceptions import WebDriverException
 
 import time
 import unittest
-
+import pdb
 
 MAXWAIT = 10
 
 
-class NewVisitorTest(LiveServerTestCase):
+class NewVisitorTest(StaticLiveServerTestCase):
     def setUp(self):
         self.browser = webdriver.Firefox()
 
